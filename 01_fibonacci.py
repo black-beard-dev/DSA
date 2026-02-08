6366294f76f841e8b95f63b8ac52366b03c86eeb
black-beard-dev <nadafsdn47@gmail.com>
2026-02-08 12:40:45 +0530

    Update 01_fibonacci.py

--- 01_fibonacci.py
+++ 01_fibonacci.py
@@ -22,10 +22,12 @@
 # That’s it. No magic.
 
 def fibonacci(n):
-    dp = [0 for i in range(n)]
+    if n <= 1:
+        return n
+    dp = [0] * (n + 1)
     dp[1] = 1
-
-    for i in range(2, n):
+    for i in range(2, n + 1):
         dp[i] = dp[i-1] + dp[i-2]
 
-    return dp[-1]
+    return dp[n]
+
